a1ea7cc3b66047b716ed592d759c80826b56d08e
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-02-25 21:01:47 +0700

    +EF: prepare base stylings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@/assets/font/basier/stylesheet.css"
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
