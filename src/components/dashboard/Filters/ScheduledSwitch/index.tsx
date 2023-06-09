71a9021c36ab8960bf3c7754187f7de5c12d3c6d
Rico Putra Pradana <rico.putra@mplus.software>
2023-06-09 16:02:20 +0700

    +T15465 feature(dashboard): add styling to the label of ScheduledSwitch component

--- src/components/dashboard/Filters/ScheduledSwitch/index.tsx
+++ src/components/dashboard/Filters/ScheduledSwitch/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Switch from '@/base-ui/Switch';
 import { Flex, Text } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import useFiltersStore from '../../stores/useFiltersStore';
 
 const ScheduledSwitch: React.FC = () => {
@@ -10,8 +10,19 @@ const ScheduledSwitch: React.FC = () => {
   
   return (
     <div id="dashboard-scheduled-switch">
-      <Flex>
-        <Text>Scheduled</Text>
+      <Flex
+        alignItems="center"
+        columnGap="5px"
+        height="26px"
+      >
+        <Text
+          fontSize="14px"
+          color="black2"
+          cursor="pointer"
+          onClick={ toggleScheduled }
+        >
+          Scheduled
+        </Text>
         <Switch
           isActive={ isScheduled }
           toggle={ toggleScheduled }
