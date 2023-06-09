1d5037e42ce8b6733e1435ffc5780a5f238ea671
Rico Putra Pradana <rico.putra@mplus.software>
2023-06-09 19:03:04 +0700

    +T15465 feature(dashboard): implement clear filter functionality

--- src/base-ui/Navbar/ClearFilterButton/index.tsx
+++ src/base-ui/Navbar/ClearFilterButton/index.tsx
@@ -1,12 +1,15 @@
+import useFiltersStore from '@/components/dashboard/stores/useFiltersStore'
 import { Button } from '@chakra-ui/react'
 import React from 'react'
 
 const ClearFilterButton: React.FC = () => {
+  const reset = useFiltersStore(state => state.reset);
   return (
     <Button
       fontSize="16px"
       color="blue"
       paddingX="10px"
+      onClick={ reset }
     >
       Clear Filter
     </Button>
